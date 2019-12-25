dc3855f7e1ad8b370a8733e69b5a5ab83bbfa2ba
Diogo.Mendonca <diogo.mendonca@hostelworld.com>
2019-12-25 23:33:52 +0000

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { SetlistModule } from './setlist/setlist.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(SetlistModule);
+  app.enableCors();
   await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
